0e2c010873c5b6a9f0ab4278d731d8afdc246fd1
amardito <amardito37@gmail.com>
2020-08-14 20:47:28 +0700

    Cart Page | Modeli

--- pages/keranjang.js
+++ pages/keranjang.js
@@ -1,11 +1,54 @@
+import Head from 'next/head';
+import { useContext } from "react";
+import Styles from "../styles/page/Cart.module.css";
 import Modalorder from "../component/modal/order.js";
+import { MCheckoutContext } from "../context/index.js";
 
 const Keranjang = () => {
-    return(
-        <div>
-            <Modalorder></Modalorder>
+
+  const [staModalCheckout] = useContext(MCheckoutContext);
+
+  let component;
+
+  if(staModalCheckout === false)  {
+    component = <div></div>
+  }
+  else{
+    component = <Modalorder></Modalorder>
+  }
+
+  return(
+    <div>
+      {component}
+
+      <Head>
+        <title>{'name product'}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      
+      <div className={Styles.FuContainer}>
+        <div className={Styles.Main}>
+          <div className={Styles.MainCon}>
+
+            <div className={Styles.Content}>
+              
+            </div>
+
+            <div className={Styles.Side}>
+              <div className={Styles.BoxCard}>
+                <div className={Styles.Card}>
+
+                </div>
+              </div>
+            </div>
+
+          </div>
         </div>
-    )
+      </div>
+
+    </div>
+  )
 }
 
 export  default Keranjang;
