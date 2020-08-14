c2869053563cd7fb77b340d6086e7c5879971eae
amardito <amardito37@gmail.com>
2020-08-15 04:58:43 +0700

    Cart Page | Responsive

--- pages/keranjang.js
+++ pages/keranjang.js
@@ -3,6 +3,9 @@ import { useContext } from "react";
 import Styles from "../styles/page/Cart.module.css";
 import Modalorder from "../component/modal/order.js";
 import { MCheckoutContext } from "../context/index.js";
+import { IoMdTrash } from "react-icons/io";
+import { BsFillPlusSquareFill } from "react-icons/bs";
+import { BsDashSquareFill } from "react-icons/bs";
 
 const Keranjang = () => {
 
@@ -31,16 +34,128 @@ const Keranjang = () => {
         <div className={Styles.Main}>
           <div className={Styles.MainCon}>
 
-            <div className={Styles.Content}>
-              
+            <div className={Styles.ConHeader}>
+              <h1>Review Your Cart</h1>
             </div>
 
-            <div className={Styles.Side}>
-              <div className={Styles.BoxCard}>
-                <div className={Styles.Card}>
+            <div className={Styles.LineHr}></div>
+
+            <div className={Styles.Content}>
+
+              <div className={Styles.SideContent}>
+
+                <div className={Styles.GroupList}>
+
+                  <div className={Styles.TittleGroup}>
+                    <h1>Nama Toko</h1>
+                  </div>
+
+                  <div className={Styles.ItemList}>
+
+                    <div className={Styles.Item}>
+                      <div className={Styles.InfoItem}>
+                        <h1>Product Name hjjsacc kjsack kscbmm asckb</h1>
+                        <p>IDR 500.000</p>
+                      </div>
+                      <div className={Styles.ItemAct}>
+                        <button>
+                          <IoMdTrash/>
+                        </button>
+                        <div className={Styles.ItemQty}>
+                          <button><BsFillPlusSquareFill/></button>
+                          <input type="number"></input>
+                          <button><BsDashSquareFill/></button>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className={Styles.Item}>
+                      <div className={Styles.InfoItem}>
+                        <h1>Product Name</h1>
+                        <p>IDR 50.000</p>
+                      </div>
+                      <div className={Styles.ItemAct}>
+                        <button>
+                          <IoMdTrash/>
+                        </button>
+                        <div className={Styles.ItemQty}>
+                          <button><BsFillPlusSquareFill/></button>
+                          <input type="number"></input>
+                          <button><BsDashSquareFill/></button>
+                        </div>
+                      </div>
+                    </div>
+
+                  </div>
 
                 </div>
+
+                <div className={Styles.GroupList}>
+
+                  <div className={Styles.TittleGroup}>
+                    <h1>Nama Toko</h1>
+                  </div>
+
+                  <div className={Styles.ItemList}>
+
+                    <div className={Styles.Item}>
+                      <div className={Styles.InfoItem}>
+                        <h1>Product Name</h1>
+                        <p>IDR 50.000</p>
+                      </div>
+                      <div className={Styles.ItemAct}>
+                        <button>
+                          <IoMdTrash/>
+                        </button>
+                        <div className={Styles.ItemQty}>
+                          <button><BsFillPlusSquareFill/></button>
+                          <input type="number"></input>
+                          <button><BsDashSquareFill/></button>
+                        </div>
+                      </div>
+                    </div>
+
+                  </div>
+
+                </div>
+
               </div>
+
+              <div className={Styles.Side}>
+                <div className={Styles.BoxCard}>
+                  <div className={Styles.Card}>
+
+                    <div className={Styles.Preview}>
+                      <img src="/image/dash2.webp"></img>
+                      <div className={Styles.PreviewDetail}>
+                        <h1>Product Namei oasdkj jkadkk njasd</h1>
+                        <p>IDR 50.000</p>
+                      </div>
+                    </div>
+
+                    <div className={Styles.Linehr}></div>
+
+                    <div className={Styles.ConPreview}>
+                      <h1>Summary Bill</h1>
+                      <div>
+                        <p>Total Bill</p>
+                        <p>IDR 150.000</p>
+                      </div>
+                    </div>
+
+                    <div className={Styles.CheckOut}>
+                      <button>CheckOut</button>
+                    </div>
+                    
+
+                  </div>
+                </div>
+              </div>
+
+              <div className={Styles.Footer}>
+                
+              </div>
+
             </div>
 
           </div>
