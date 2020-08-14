0d5d4f1dd4136d30097bede7c7d84a3df1e78097
Indra <me@aboutindra.com>
2020-08-14 18:41:12 +0700

    Update Layout

--- component/modal/order.js
+++ component/modal/order.js
@@ -12,27 +12,51 @@ const Modalorder = () => {
                             <AiOutlineLeft></AiOutlineLeft>
                             <AiFillShopping></AiFillShopping>
                         </div>
-                        <h3 className={styles.title}>Ringkasan</h3>
+                        <h3 className={styles.title}>Summary</h3>
                         <div className={styles.itemList}>
                             <div className={styles.itemOrder}>
                                 <img className={styles.imgOrder} src="/image/adidas.webp"></img>
                                 <div className={styles.totalOrder}>
-                                    <h4>Adidas Superstar KW Super</h4>
-                                    <label className={styles.total}> Total :  1 </label>
+                                    <h4 className={styles.itemTitle}>Adidas Superstar</h4>
+                                    <div className={styles.itemTotal}> Total :  1 </div>
+                                    <h3 className={styles.itemPrice}>IDR. 105K</h3>
                                 </div>
-                                <label></label>
+                            </div>
+                            <div className={styles.itemOrder}>
+                                <img className={styles.imgOrder} src="/image/adidas.webp"></img>
+                                <div className={styles.totalOrder}>
+                                    <h4 className={styles.itemTitle}>Adidas Superstar</h4>
+                                    <div className={styles.itemTotal}> Total :  1 </div>
+                                    <h3 className={styles.itemPrice}>IDR. 105K</h3>
+                                </div>
+                            </div>
+                            <div className={styles.itemOrder}>
+                                <img className={styles.imgOrder} src="/image/adidas.webp"></img>
+                                <div className={styles.totalOrder}>
+                                    <h4 className={styles.itemTitle}>Adidas Superstar</h4>
+                                    <div className={styles.itemTotal}> Total :  1 </div>
+                                    <h3 className={styles.itemPrice}>IDR. 105K</h3>
+                                </div>
+                            </div>
+                            <div className={styles.itemOrder}>
+                                <img className={styles.imgOrder} src="/image/adidas.webp"></img>
+                                <div className={styles.totalOrder}>
+                                    <h4 className={styles.itemTitle}>Adidas Superstar</h4>
+                                    <div className={styles.itemTotal}> Total :  1 </div>
+                                    <h3 className={styles.itemPrice}>IDR. 105K</h3>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className={styles.cardBottom}>
                         <div className={styles.cardBody}>
                             <div className={styles.subtotal}>
-                                <div>Subtotal</div>
-                                <div>IDR. 105.000,00</div>
+                                <h4>Subtotal</h4>
+                                <h4 className={styles.totalPrice}>IDR. 105.000,00</h4>
                             </div>
-
+                            <hr className={styles.line}></hr>
                             <div className={styles.payment}>
-                                <div className={styles.title}>
+                                <div className={styles.subtitle}>
                                     <p>CURRENT METHOD</p>
                                 </div>
 
@@ -41,8 +65,8 @@ const Modalorder = () => {
                                         <div className={styles.images}>
                                             <img src="/image/gopay.png" />
                                         </div>
-                                        <div className={styles.title}>
-                                            <p>GOPAY</p>
+                                        <div className={styles.subtitle}>
+                                            <p>Using Payroll</p>
                                             <p className={styles.deskripsi}>Default Method</p>
                                         </div>
                                         <div className={styles.radio}>
@@ -51,8 +75,8 @@ const Modalorder = () => {
                                     </div>
                                 </label>
 
-                                <div className={styles.button}>
-                                    <button type="submit">Finish Order</button>
+                                <div >
+                                    <button className={styles.button} type="submit">Finish Order</button>
                                 </div>
                             </div>
                         </div>
